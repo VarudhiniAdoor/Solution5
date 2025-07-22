ab76ed7fd07063fcb52b4bd64f055d70efd905b2
Anamika Sawhney <anamika.sawhney22@gmail.com>
2025-07-22 14:20:48 +0530

    Add project files.

---
+++ MiscTopics/EnumDemo.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiscTopics
+{
+    enum Choice : byte { Add = 1, Subtract, Product, Remainder };
+    internal class EnumDemo
+    {
+        // WAP to take 2 numbers & choice
+        // choice 1 > add 
+        // 2 > -
+        // 3 - product
+        // 4 - remainder
+        static void Main()
+        {
+            int num1, num2, ch;
+            Console.WriteLine("enter num1");
+            num1 = byte.Parse(Console.ReadLine());
+            Console.WriteLine("enter num2");
+            num2 = byte.Parse(Console.ReadLine());
+            Console.WriteLine("enter choice");
+            ch = byte.Parse(Console.ReadLine());
+            //switch (ch)
+            //{
+            //    case 1: Console.WriteLine(num1 + num2); break;
+            //    case 2: Console.WriteLine(num1 - num2); break;
+            //    case 3: Console.WriteLine(num1 * num2); break;
+            //    case 4: Console.WriteLine(num1 % num2); break;
+            //    default: Console.WriteLine("invalid choice");break;
+            //}
+            switch (ch)
+            {
+                case (int)Choice.Add: Console.WriteLine(num1 + num2); break;
+                case (int)Choice.Subtract: Console.WriteLine(num1 - num2); break;
+                case (int)Choice.Product: Console.WriteLine(num1 * num2); break;
+                case (int)Choice.Remainder: Console.WriteLine(num1 % num2); break;
+                default: Console.WriteLine("invalid choice"); break;
+            }
+
+
+
+
+
+        }
+    }
+}
---
+++ MiscTopics/Program.cs
@@ -0,0 +1,69 @@
+﻿namespace MiscTopics
+{
+    struct Record
+    {
+        int id;
+        string name;
+        string dept;
+        //public Record() { }
+        //public Record (int id)
+        //{
+
+        //this.id = id;}
+
+        public Record(int id, string name, string dept)
+        {
+            this.id = id;
+            this.name = name;
+            this.dept = dept;
+           
+        }
+        public void Display()
+        {
+            Console.WriteLine("ID is " + id);
+            Console.WriteLine("Name is " + name);
+            Console.WriteLine("Dept is "  + dept);
+        }
+    }
+
+    struct Address
+    {
+        int houseNo;
+        string city, state, pincode, mobileNum;
+        public void DisplayAddress()
+        {
+            Console.WriteLine("house no  " + houseNo + " " + city+ " "+ state );
+        }
+    }
+    class Employee
+    {
+         int id;
+         string name;
+         Address address;
+        public void Display()
+        {
+            Console.WriteLine("ID" + id);
+            Console.WriteLine("Address ");
+            address.DisplayAddress();
+        }
+        
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            int? num = 10;
+            if (num.HasValue)
+            {
+                Console.WriteLine(num.Value);
+            }
+            else
+                Console.WriteLine("num contains null");
+            Record rec = new Record(100, "ajay", "accts");
+            rec.Display();
+            Employee employee = new Employee();
+            employee.Display(); 
+        }
+    }
+}
