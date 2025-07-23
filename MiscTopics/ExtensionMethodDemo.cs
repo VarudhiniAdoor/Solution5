71f5d83c622313e52375609e7b448ac04cb631b8
Anamika Sawhney <anamika.sawhney22@gmail.com>
2025-07-23 12:37:37 +0530

    added extension methods

---
+++ MiscTopics/ExtensionMethodDemo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiscTopics
+{
+    internal class A
+    {
+        public void Get1()
+        {
+            Console.WriteLine("Get1 of Actual class");
+        }
+    }
+
+    // extension meethods
+    static class ExtendedA
+    {
+        public static void Get2(this A a)
+        {
+            Console.WriteLine("Method Get2 has been added to class A");
+        }
+    }
+    class Prog
+    {
+        static void Main(string[] args)
+        {
+            A a = new A();
+            a.Get1();
+            a.Get2();
+        }
+
+    }
+}
